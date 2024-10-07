a02a9ef75f114eed4c06d5c6f1b490bad1efd27a
ElenaCherpakova <elena.cherpakova@gmail.com>
2024-10-06 23:19:02 -0400

    update: add reject and replace promise.all to promise allsettled

--- src/wt/main.js
+++ src/wt/main.js
@@ -6,13 +6,15 @@ import os from 'node:os';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const workerFile = path.resolve(__dirname, './worker.js');
-const qntOfCPU = os.cpus().length;
-const results = new Array(qntOfCPU).fill(null);
+
 const performCalculations = async () => {
   const promises = [];
+  const qntOfCPU = os.cpus().length;
+  const results = new Array(qntOfCPU).fill(null);
+  console.log(results);
   for (let i = 0; i < qntOfCPU; i++) {
     const n = 10 + i;
-    const promise = new Promise((resolve) => {
+    const promise = new Promise((resolve, reject) => {
       const worker = new Worker(workerFile);
       worker.postMessage(n);
 
@@ -27,13 +29,15 @@ const performCalculations = async () => {
       worker.on('exit', (code) => {
         if (code !== 0) {
           results[i] = { status: 'error', data: null };
-          resolve();
+          reject(new Error(
+            `Worker ${i} stopped with exit code ${code}`,
+          ));
         }
       });
     });
     promises.push(promise);
   }
-  await Promise.all(promises);
+  await Promise.allSettled(promises);
   console.log(results);
 };
 await performCalculations();
